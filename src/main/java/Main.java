86360bc0c423ce80925bdb6aaa926ad74a569288
Wojciech Łoś <wojtek.los2003@gmail.com>
2022-06-07 14:15:42 +0200

    Nowy projekt

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(readingFile.main(args));
+        Wystawca imieniem = new Wystawca("mBank Sp.z.o.o.",12334567, "Głębocka",24,"03-212","Warszawa");
+
+        System.out.println(imieniem);
+
+
+    }
+
+}
